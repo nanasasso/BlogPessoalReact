e9b34d935675b5617448bd550f78eff5e95c8f76
Fernanda Sasso <fsasso.23@gmail.com>
2024-02-29 14:55:36 -0300

    deploy

--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
+import { DNA } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthContext';
 import Tema from '../../../models/Tema';
@@ -41,7 +41,7 @@ function ListaTemas() {
   return (
     <>
       {temas.length === 0 && (
-        <Dna
+        <DNA
           visible={true}
           height="200"
           width="200"
