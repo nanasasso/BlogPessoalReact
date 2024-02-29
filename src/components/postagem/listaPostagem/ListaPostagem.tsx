bdfd7d15aca27698f3f8846b9ad9f95633362678
Fernanda Sasso <fsasso.23@gmail.com>
2024-02-29 14:51:22 -0300

    deploy

--- src/components/postagem/listaPostagem/ListaPostagem.tsx
+++ src/components/postagem/listaPostagem/ListaPostagem.tsx
@@ -1,5 +1,5 @@
-import  { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
+import { useContext, useEffect, useState } from 'react';
+import { DNA } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthContext';
 import Postagem from '../../../models/Postagem';
@@ -44,7 +44,7 @@ function ListaPostagens() {
   return (
     <>
       {postagens.length === 0 && (
-        <Dna
+        <DNA
           visible={true}
           height="200"
           width="200"
